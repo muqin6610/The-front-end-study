bb29130fa09aa4385291f6a85cda3ba3c75c88e9
biao.qin <biao.qin@aiwinn.com>
2020-01-13 09:45:07 +0800

    服务器分离

--- service/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const services = require('./services.js')
-router.get('/', services.start)
-// 登录功能
-router.get('/api/login', services.login)
-// 注册功能
-router.post('/api/register', services.register)
-module.exports = router
