8aef21cbedb87edbbd135ee10d96ff417b57e7f2
biao.qin <biao.qin@aiwinn.com>
2020-01-13 15:23:36 +0800

    用户年龄段监控数据获取完成

--- src/components/Charts/userMonitoringCharts/vePieChart.vue
+++ src/components/Charts/userMonitoringCharts/vePieChart.vue
@@ -1,29 +1,35 @@
 <template>
   <div>
-    <ve-pie :data="chartData1" :colors="colors"></ve-pie>
+    <ve-pie :data="chartData" :colors="colors"></ve-pie>
   </div>
 </template>
 
 <script>
+import { getApi } from '@/api/api.js'
+
 export default {
     data() {
         this.colors = ['#36a3f7', '#d53a35', '#334b5c', '#6ab0b8', '#e98f6f', '#9fdabf']
         return {
-          chartData1: {
+          chartData: {
             columns: ['年龄段', '用户'],
-            rows: [
-              { '年龄段': '18岁以下', '用户': 1393 },
-              { '年龄段': '18-25岁', '用户': 2015 },
-              { '年龄段': '25-30岁', '用户': 2350 },
-              { '年龄段': '30-40岁', '用户': 1620 },
-              { '年龄段': '40-50岁', '用户': 1210 },
-              { '年龄段': '50岁以上', '用户': 856 }
-            ]
+            rows: []
           }
         }
     },
+    created() {
+      // 获取用户年龄段监控数据
+      this.getUserAge()
+    },
     methods: {
-        
+      // 获取用户年龄段监控数据
+      async getUserAge() {
+        let res = await getApi('getUserAge', null)
+        console.log(res,'用户年龄段数据')
+        if(res.success) {
+          this.chartData.rows = res.result
+        }
+      }
     },
 }
 </script>
