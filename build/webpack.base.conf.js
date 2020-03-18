6fa37087229c31208bc6e1902db10de87404fb40
biao.qin <biao.qin@aiwinn.com>
2020-03-18 09:56:25 +0800

    解决编译错误

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -65,6 +65,10 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
+      { //从这一段上面是默认的！不用改！下面是没有的需要你手动添加，相当于是编译识别sass! 
+         test: /\.scss$/,
+         loaders: ["style", "css", "sass"]
+      }
     ]
   },
   externals: {
